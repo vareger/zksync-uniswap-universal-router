c56572cc320920044244f0c59694c3c80f9a6fd1
AntonD3 <antonv.dyadyuk@gmail.com>
2023-05-12 09:55:06 +0200

    zkSync Testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
 import 'hardhat-typechain'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
+import '@matterlabs/hardhat-zksync-deploy'
+import '@matterlabs/hardhat-zksync-solc'
+import '@matterlabs/hardhat-zksync-verify'
+import { task } from 'hardhat/config'
+import deployZkSyncEra from './script/deploy_zksync_era'
 import dotenv from 'dotenv'
 dotenv.config()
+
+task('deployZkSyncEra')
+    .addParam('privateKey', 'Private key used to deploy')
+    .addParam('params', "Path to params json")
+    .addFlag('verify', 'Whether verify the contracts')
+    .setAction(async (taskArgs) => {
+      await deployZkSyncEra(taskArgs)
+    })
 
 const DEFAULT_COMPILER_SETTINGS = {
   version: '0.8.17',
@@ -59,11 +72,31 @@ export default {
     optimism: {
       url: `https://mainnet.optimism.io`,
     },
+    zkSyncLocalhost: {
+      url: "http://localhost:3050",
+      ethNetwork: "http://localhost:8545",
+      zksync: true,
+    },
+    zkSyncTestnet: {
+      url: "https://testnet.era.zksync.dev",
+      ethNetwork: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      zksync: true,
+      verifyURL: 'https://zksync2-testnet-explorer.zksync.dev/contract_verification'
+    },
   },
   namedAccounts: {
     deployer: 0,
   },
   solidity: {
     compilers: [DEFAULT_COMPILER_SETTINGS],
+  },
+  zksolc: {
+    version: "1.3.10",
+    compilerSource: "binary",
+    settings: {
+      metadata: {
+        bytecodeHash: 'none',
+      },
+    },
   },
 }
